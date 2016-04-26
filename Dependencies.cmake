7678a91776433cb202ef969bf10484aa34b83a49
Iceberg YOUNG <iceberg.young@gmail.com>
2016-04-27 00:44:58 +0800

    Moved from CMake to ninja build.

--- Dependencies.cmake
+++
@@ -1,3 +0,0 @@
-add_subdirectory(import/datetime/source)
-add_dependencies(${PROJECT_NAME} so.datetime)
-target_link_libraries(${PROJECT_NAME} so.datetime)
--- suit.cpp
+++ suit.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <unistd.h>
-#include "include/log.hpp"
+#include "log.hpp"
 
 using namespace std;
 using namespace so;
 
-void generate(log_filter::label label) {
+void generate(log_label label) {
     log x{label};
-    x.append_tag("LGPL");
+    x.assign("LGPL");
     x << "This program is free software: you can redistribute it and/or modify it\n"
       "under the terms of the GNU Lesser General Public License as published by\n"
       "the Free Software Foundation, either version 3 of the License, or\n"
@@ -18,18 +18,18 @@ void generate(log_filter::label label) {
       "GNU Lesser General Public License for more details.\n\n";
     x << "You should have received a copy of the GNU Lesser General Public License\n"
       "along with this program.  If not, see <http://www.gnu.org/licenses/>.";
-    x.append_tag("v3");
+    x.assign("v3");
     usleep(rand() % 7000);
-    cerr << x.str() << endl;
+    cerr << x << endl;
 }
 
 int main() {
-    generate(log_filter::label::special);
-    generate(log_filter::label::failure);
-    generate(log_filter::label::warning);
-    generate(log_filter::label::caution);
-    generate(log_filter::label::message);
-    generate(log_filter::label::success);
-    generate(static_cast<log_filter::label>(13));
+    generate(log_label::special);
+    generate(log_label::failure);
+    generate(log_label::warning);
+    generate(log_label::caution);
+    generate(log_label::success);
+    generate(log_label::verbose);
+    generate(static_cast<log_label>(13));
     return 0;
 }
